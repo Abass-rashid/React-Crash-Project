fd80d8a115560661d6b754b662eb192340cafa67
Abazrashid <abazrashid144@gmail.com>
2023-09-29 17:28:33 +0300

    Add styles to Post component

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -1,10 +1,10 @@
-
+import classes from "./Post.module.css"
 const Post = ({name, value}) => {
  
     return ( 
-    <div>
-      <p>{name}</p>
-      <h3>{value}</h3>
+    <div className={classes.Post}>
+      <p className={classes.author}>{name}</p>
+      <h3 className={classes.text}>{value}</h3>
     </div>
    );
 }
