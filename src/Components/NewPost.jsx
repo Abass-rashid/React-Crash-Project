f8c55c7522df11c71ef4a2ccf7567fe4868c455f
Abazrashid <abazrashid144@gmail.com>
2023-09-30 06:57:59 +0300

    refractored auther state

--- src/Components/NewPost.jsx
+++ src/Components/NewPost.jsx
@@ -1,16 +1,16 @@
 
 import classes from "./NewPost.module.css"
-const NewPost = () => {
+const NewPost = ({handleChange,onAutherChange}) => {
  
   return (
     <form className={classes.form}>
        <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} />
+        <textarea id="body" required rows={3} onChange={handleChange}/>
        </p>
        <p>
         <label htmlFor="body">Your Name.</label>
-        <input id="name"  type="text"  required />
+        <input id="name"  type="text"  required onChange={onAutherChange}/>
        </p>
     </form>
     );
--- src/Components/PostList.jsx
+++ src/Components/PostList.jsx
@@ -1,18 +1,25 @@
-
+import {useState} from "react"
 import Post from "./Post";
 import classes from "./PostList.module.css"
 import NewPost from "./NewPost";
 
 const PostList = () => {
-  
+  const [enteredBody, setEnteredBody] = useState("")
+  const [enteredAuther, setEnteredAuther] = useState("");
+  const handleChange = (event) => {
+    setEnteredBody(event.target.value)
+}
+  const handleAuther = (event) => {
+    setEnteredAuther(event.target.value)
+}
   return ( 
     <>
         
-    <NewPost/>
+    <NewPost  handleChange={handleChange} onAutherChange={handleAuther} />
     <ul className={classes.posts}>
          
    <Post name ="Abasss rashid"  value="am learning React!"  /> 
-   <Post name ="Jafar Ibrahim" value= "am learning React!"  />    
+   <Post name ={enteredAuther} value= {enteredBody}  />    
    <Post name ="Hussein Osman"  value="I love Python js"  />   
      
    </ul></>
